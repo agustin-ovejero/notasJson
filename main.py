3dd64f9ece6241d5894cbba078a4dcf55e7f5d2f
agustin-ovejero <ovejeroagustin2@gmail.com>
2026-01-27 16:02:01 -0300

    creando la ruta para seleccionar las notas por su id

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from flask import Flask
-from flask import jsonify, request
+from sqlite3.dbapi2 import DatabaseError
+
+from flask import Flask, jsonify, request
+
 from database import db
 from models import Notas
 
@@ -9,16 +11,28 @@ db.init_app(app)
 with app.app_context():
     db.create_all()
 
-@app.route('/')
+
+@app.route("/")
 def index():
     return "<h1>hello world<h1"
 
-@app.route('/seenote', methods=['GET'])
+
+@app.route("/seenote", methods=["GET"])
 def ver_nota():
-    notas = Notas.query.all() # esta manera obtenemos todos las notas de la db
+    notas = Notas.query.all()  # esta manera obtenemos todos las notas de la db
     en_json = [{"id": i.id, "titulo": i.titulo, "nota": i.nota} for i in notas]
 
     return jsonify(en_json)
+
+
+@app.route("/idnote/<int:id>", methods=["GET"])
+def nota_id(id):
+
+    try:
+        nota = Notas.query.get(id)
+        return jsonify({"id": nota.id, "titulo": nota.titulo, "nota": nota.nota}), 200
+    except ValueError:
+        return jsonify({"mensje": "algo salio mal"})
 
 
 @app.route("/delnote/<titulo>", methods=["DELETE"])
@@ -37,19 +51,20 @@ def eliminando_nota(titulo):
         return jsonify({"mensaje": "error db"})
 
 
-
-@app.route('/makenote', methods=["POST"])
+@app.route("/makenote", methods=["POST"])
 def crear_nota():
-    datos = request.get_json() # de esta manera obtenemos los datos pasados en formato json
+    datos = (
+        request.get_json()
+    )  # de esta manera obtenemos los datos pasados en formato json
 
-    nueva_nota = Notas( # de esta manera creamos el dato que envuelve todos los datos de para la base
-        titulo = datos['titulo'],
-        nota = datos['nota']
+    nueva_nota = Notas(  # de esta manera creamos el dato que envuelve todos los datos de para la base
+        titulo=datos["titulo"],
+        nota=datos["nota"]
     )
 
     try:
-        db.session.add(nueva_nota) # agregamos a la base
-        db.session.commit() # guardamos en la base
+        db.session.add(nueva_nota)  # agregamos a la base
+        db.session.commit()  # guardamos en la base
         return jsonify({"mensaje": "nota creada"}), 201
     except ValueError:
         db.session.rollback()
