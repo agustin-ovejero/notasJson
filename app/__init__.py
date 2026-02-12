4428aead53ae39f347f8104114cbb589074498ff
agustin-ovejero <ovejeroagustin2@gmail.com>
2026-02-12 11:17:19 -0300

    creando en notas_bp.py la ruta y funcion para la creacion de notas y en __init__.py enlazando el archivo notas_bp.py a la app

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,11 @@ from .extensions import db
 from config import Config
 
 from .routes.index import index
+from .routes.notas_bp import notes
 def create_app():
    app = Flask(__name__) 
    app.register_blueprint(index)
+   app.register_blueprint(notes)
    
    
    app.config.from_object(Config) # El metodo from_object solamente acepta clases que las va a escanear
--- app/routes/notas_bp.py
+++ app/routes/notas_bp.py
@@ -7,15 +7,19 @@ notes = Blueprint('notas', __name__)
 
 # Nuestro CRUD
 @notes.route('/makenote', methods=['POST']) 
-def makenote():
+def makenotes():
     datos = request.get_json()
     
-    nueva_nota = Notas(titulo = datos["titulo"], nota = dato["nota"])
+    #nueva_nota = Notas(titulo=datos["titulo"], nota=datos["nota"])
     try:
+        nueva_nota = Notas(titulo=datos["titulo"], nota=datos["nota"])
         db.session.add(nueva_nota)
         db.session.commit()
         return jsonify({"mensaje": "Nota creada"}), 201
-    except ValueError:
+    except KeyError as key:
         db.session.rollback()
-        return jsonify({"mensaje": "ocurrio un erro"}), 400
+        return jsonify({"mensaje": f"Falta agregar los datos para la siguiente clave: {key}"}), 400
+    finally:
+        db.session.rollback()
+        return jsonify({"mensaje": "Ocurrio un error inesperado"}), 400
      
